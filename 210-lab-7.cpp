2e27d33556de0cdbe76506b353d551e3b0e89b1b
Keshav Yanamandra <keshav.yanamandra@gmail.com>
2026-09-13 16:50:40 -0700

    created dynamic string array

--- 210-lab-7.cpp
+++ 210-lab-7.cpp
@@ -3,13 +3,37 @@
 // Lab 7
 
 #include <iostream>
+#include <string>
+
 using namespace std;
+
+const int SIZE = 5;
 
 void displayArray(string*);
 string* reverseArray(string*);
 
 
 int main (){
+
+    // creating dynamic array
+    string *data = nullptr;
+    data = new string[SIZE];
+
+    // add names with pointer 
+    *(data + 0) = "Janet";
+    *(data + 1) = "Jeffe";
+    *(data + 2) = "Jin";
+    *(data + 3) = "Joe";
+    *(data + 4) = "Junio";
+
+    cout << *(data + 0) << " ";
+    cout << *(data + 1) << " ";
+    cout << *(data + 2) << " ";
+    cout << *(data + 3) << " ";
+    cout << *(data + 4) << endl;
+
+    // delete array when done
+    delete [] data;
 
     return 0;
 }
